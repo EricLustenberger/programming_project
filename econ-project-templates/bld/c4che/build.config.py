3990d7826cbd103be5bb6d4c4a44e557092cae07
Eric Lustenberger <eric.lustenberger@hotmail.com>
2017-03-22 14:34:26 +0100

    Added matlab template.

--- econ-project-templates/bld/c4che/build.config.py
+++
@@ -1,2 +0,0 @@
-version = 0x1090500
-tools = [{'funs': None, 'tool': 'biber', 'tooldir': None}, {'funs': None, 'tool': 'run_py_script', 'tooldir': None}, {'funs': None, 'tool': 'tex', 'tooldir': None}, {'funs': None, 'tool': 'sphinx_build', 'tooldir': None}, {'funs': None, 'tool': 'write_project_headers', 'tooldir': None}]
